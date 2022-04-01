36b37c2a1f0110030279af3a49dc46f5f7aaa1d2
oliveiracustodioevelyn <102164220+oliveiracustodioevelyn@users.noreply.github.com>
2022-04-01 16:49:18 -0400

    Add files via upload

---
+++ ativ1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ativ1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string usuario = TxtEmail.Text;
+            string senha = TxtSenha.Password.ToString();
+            if (usuario == "evelyncustodio321@gmail.com")
+                MessageBox.Show("E-mail Válida", "Confirmação de Login", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+            else
+                MessageBox.Show("E-mail Inválido", "Confirmação de Login", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+
+            if (senha == "123")
+                MessageBox.Show("Senha Válida", "Confirmação de Login", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+            else
+                MessageBox.Show("Senha Inválida", "Confirmação de Login", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+
+            TelaInicial tela = new TelaInicial();
+            tela.ShowDialog();
+
+            
+        }
+
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+    }
+}
